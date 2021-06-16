c8821c5508a5f6b249fc58a45763d68f28723fe5
bagussona <bagussona87@gmail.com>
2021-06-16 18:42:06 +0700

    Bagus-> User Management rev.1

---
+++ app/Http/Controllers/api/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+        $credentials = $request->only('username', 'password');
+
+        try{
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 400);
+                }
+            } catch (JWTException $e){
+
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        return response()->json(compact('credentials', 'token'));
+    }
+
+    public function register(Request $request){
+        // dd($request);
+        $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:100',
+        'username' => 'required|string|max:20|unique:users',
+        'email' => 'required|string|email|max:50|unique:users',
+        'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $customer = User::create([
+            'name' => $request->get('name'),
+            'username' => $request->get('username'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+
+        return response()->json(compact('customer'), 201);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+
+        }
+
+        return response()->json(compact('user'));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('register', 'api\UserController@register');
+Route::post('login', 'api\UserController@login');
+
+Route::get('profile', 'api\UserController@getAuthenticatedUser')->middleware('jwt.verify');
