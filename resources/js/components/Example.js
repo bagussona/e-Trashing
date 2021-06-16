ffe2d5bee91de92242fc819e5e5437e2ea79d723
ziunadev <krishna.putra332@gmail.com>
2021-06-16 08:11:46 +0700

    wsl: first try with react Component

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -19,6 +19,6 @@ function Example() {
 
 export default Example;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById('app')) {
+    ReactDOM.render(<Example />, document.getElementById('app'));
 }
