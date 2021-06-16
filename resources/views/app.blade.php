cf36bb6ab5491c8ccaa641fb587d3102e9a4015b
ziunadev <krishna.putra332@gmail.com>
2021-06-16 08:27:23 +0700

    wsl: add some another configuration & edit some files

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Styles -->
         <!-- <style>
             html, body {
