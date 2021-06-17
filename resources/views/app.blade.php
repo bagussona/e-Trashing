99e69041c835895942db3008dabcda5cbddcba73
ziunadev <krishna.putra332@gmail.com>
2021-06-17 21:09:25 +0700

    wsl: change title a little bit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>BTS ID | Bank Tukar Sampah</title>
+        <title>Bank Tukar Sampah | Tukar Sampahmu, Bantu Kita Selamatkan Bumi</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <!-- <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet"> -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Styles -->
         <!-- <style>
