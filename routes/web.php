320ad8021b5ee403020a802a319c8a61f5ba0bf0
ziunadev <krishna.putra332@gmail.com>
2021-06-17 21:47:07 +0700

    wsl: fixing some bugs within laravel that related to react-router-dom

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('app');
-});
+// Route::get('/', function () {
+//     return view('app');
+// });
+
+Route::get('/{path?}', [
+  'uses' => 'ReactController@show', 
+  'as' => 'app', 
+  'where' => ['path' => '.*']
+]);
 
 Auth::routes();
 
