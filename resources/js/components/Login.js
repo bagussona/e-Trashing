a3e592b194ff403aa515adfc859a26db77353016
ziunadev <krishna.putra332@gmail.com>
2021-06-18 17:06:45 +0700

    wsl: some auto generated codes

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 
 function Login() {
+
+  const login = () => {
+    const formData = new FormData();
+    // formData.append('name', 'customer');
+    formData.append('username', 'admin');
+    // formData.append('email', 'sayaganteng@gantengsekali.com');
+    formData.append('password', 'adminadmin');
+    // formData.append('password_confirmation', '123123123')
+
+    fetch(
+      'http://localhost:8000/api/login', 
+      {
+        method: 'POST',
+        body: formData
+      }
+    )
+    .then(async res => {
+      const body = await res.json();
+      console.log(body)
+    })
+    .catch(err => console.error(err))
+  }
+
+  useEffect(() => {
+    login();
+  })
+
   return (
     <>
       <div>Login</div>
