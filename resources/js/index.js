e2276c1d585909a8b0743f236d0d9b3301a9f918
ziunadev <krishna.putra332@gmail.com>
2021-06-19 11:43:22 +0700

    wsl: edit some react root codes for cleaner code

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-if (document.getElementById('app')) {
+// if (document.getElementById('app')) {
   ReactDOM.render(<App />, document.getElementById('app'))
-}
+// }
 
 if (process.env.NODE_ENV === 'development') {
   if (module.hot) {
